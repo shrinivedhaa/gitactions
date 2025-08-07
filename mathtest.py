c341408f62f64125cbd06f28ad58daa330a45907
Shrinivedhaa R <shrinivedhaa@gmail.com>
2025-08-07 15:52:29 +0530

    Update mathtest.py

--- mathtest.py
+++ mathtest.py
@@ -2,7 +2,7 @@ from hello import hello
 
 def test_hello():
     assert hello() == "Hello, World!"
-    assert hello() == 'hello world!"
+    assert hello() == "hello world!"
  
 assert 2 + 2 == 4
 assert 2 + 3 == 5
